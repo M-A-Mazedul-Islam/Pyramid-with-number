95dd03d715254c573e296da05c103a2ca82cacad
Mazedul <mazedulislam4970@gmail.com>
2017-03-21 00:36:43 +0600

    First commit

---
+++ cmake-build-debug/CMakeFiles/angle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/angle.dir/main.c.o"
+  "angle.pdb"
+  "angle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/angle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
